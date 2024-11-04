209514b8f38ab7828f6b39ec60303203d8f27fbf
Olsen Kraja <krajaolsen@gmail.com>
2024-11-04 21:17:26 +0100

    Added login and signup pages

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute inset-0 bg-cover -z-20" style="background-image: url('/images/bg-image.png')">
-    <img alt="" class="absolute inset-0 bg-cover opacity-90 -z-10" src="/images/bg.png">
+    <img alt="" class="absolute inset-0 bg-cover opacity-90 -z-10 w-full" src="/images/bg.png">
     <slot />
   </div>
 </template>
